aac4b143207db4dfc776065d209e7331bb706749
Edward Edberg <edward.edberg.halim@gmail.com>
2022-03-16 13:07:13 -0700

    add toggle show/hide addRecipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -2,7 +2,9 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import bookmarksView from './views/bookmarksView.js';
 import paginationView from './views/paginationView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 // import icons from '../img/icons.svg';
 
@@ -23,10 +25,11 @@ const controlRecipes = async function () {
 
     // 0) Update results view to mark selected search result
     resultsView.update(model.getSearchResultsPage());
+    bookmarksView.update(model.state.bookmarks);
+
     // 1 Loading recipe
     await model.loadRecipe(id);
-    // const { recipe } = model.state;
-    // console.log(recipe);
+
     // 2) Rendering recipe
     recipeView.render(model.state.recipe);
 
@@ -34,6 +37,7 @@ const controlRecipes = async function () {
   } catch (err) {
     // console.log(err);
     recipeView.renderError();
+    console.log(err);
   }
 };
 
@@ -79,19 +83,37 @@ const controlServings = function (newServings) {
 };
 
 const controlAddBookmark = function () {
+  // 1) Add/remove bookmark
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.deleteBookmark(model.state.recipe.id);
   console.log(model.state.recipe);
+
+  // 2) Update recipe view
   recipeView.update(model.state.recipe);
+
+  // 3) Render bookmarks
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+
+const controlAddRecipe = function (newRecipe) {
+  console.log(newRecipe);
+  // Upload new recipe data
 };
 
 const init = function () {
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
+
 init();
 // showRecipe();
 
